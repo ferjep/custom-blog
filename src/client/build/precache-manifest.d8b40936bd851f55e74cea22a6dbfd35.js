834e864768732474c0fd7e640e666a9cc1dff33a
Fernando Esparragoza <fernando.fjep@gmail.com>
2020-04-07 19:54:21 -0400

    heroku branch added for deployment

---
+++ src/client/build/precache-manifest.d8b40936bd851f55e74cea22a6dbfd35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3425b4b178b3fb15a97783f551940c38",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf00a3a14dd765677833",
+    "url": "/static/css/main.ad0d36b7.chunk.css"
+  },
+  {
+    "revision": "3a2f385d44ea437eba9e",
+    "url": "/static/js/2.d13ad3f1.chunk.js"
+  },
+  {
+    "revision": "d76814a92b42ad59e07c93e2ffdd2d0f",
+    "url": "/static/js/2.d13ad3f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf00a3a14dd765677833",
+    "url": "/static/js/main.dd0836ca.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
